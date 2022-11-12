96d898164c32fbc1419faa953b8bf3145630811c
Jasser Mark Arioste <jassermark.arioste@gmail.com>
2022-11-12 15:27:38 +0800

    Add props to component

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const TableOfContents = () => {
+type Heading = {
+  text: string;
+  level: 2 | 3 | 4; //h2-h4
+  id?: string;
+};
+
+type Props = {
+  headings: Heading[];
+};
+
+const TableOfContents = (props: Props) => {
   return <div>TableOfContents</div>;
 };
 
